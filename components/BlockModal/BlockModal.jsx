f64f2806b6888f94759e0bc04281ce789c7de330
WWork777 <REPetitorAv1to@ya.ru>
2025-12-06 04:09:07 +0300

    fix banner 06/12

--- components/BlockModal/BlockModal.jsx
+++ components/BlockModal/BlockModal.jsx
@@ -1,21 +1,21 @@
-'use client'
+'use client';
 import { useEffect, useState } from 'react';
 import './BlockModal.scss';
 
 const BlockModal = ({ allowClose = false }) => {
   const [isVisible, setIsVisible] = useState(true);
   const [currentDate, setCurrentDate] = useState('');
-  
+
   useEffect(() => {
     // Форматирование текущей даты
     const now = new Date();
     const formattedDate = now.toLocaleDateString('ru-RU', {
       day: 'numeric',
       month: 'long',
-      year: 'numeric'
+      year: 'numeric',
     });
     setCurrentDate(formattedDate);
-    
+
     // Блокировка скролла
     document.body.style.overflow = isVisible ? 'hidden' : 'auto';
     return () => {
@@ -26,19 +26,28 @@ const BlockModal = ({ allowClose = false }) => {
   if (!isVisible) return null;
 
   return (
-    <div className="modalOverlay">
-      <div className="modalContent">
+    <div className='modalOverlay'>
+      <div className='modalContent'>
         {allowClose && (
-          <button 
-            className="closeButton"
+          <button
+            className='closeButton'
             onClick={() => setIsVisible(false)}
-            aria-label="Закрыть уведомление"
+            aria-label='Закрыть уведомление'
           >
             &times;
           </button>
         )}
-        <h2>Сайт временно не работает</h2>
-        <p>Сегодня <strong>{currentDate}</strong> наш магазин по техническим причинам не работает. Принимаем заказы на завтра и желаем всем отличного дня!</p>
+        {/* <h2>Сайт временно не работает</h2> */}
+        {/* <p>
+          Сегодня <strong>{currentDate}</strong> наш магазин по техническим причинам не работает.
+          Принимаем заказы на завтра и желаем всем отличного дня!{' '}
+        </p> */}
+        <h2>Сайт возобновляет свою работу!</h2>
+        <p>
+          Сегодня <strong>{currentDate}</strong> наш магазин начнет работать с{' '}
+          <strong>14:00</strong> по московскому времени. Желаем всем отличного
+          дня!{' '}
+        </p>
       </div>
     </div>
   );
