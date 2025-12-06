9267314433f8b4a38c98d4d8da1d7d3d1b654a4e
id0ntc4re <parsikovevgenij470@gmail.com>
2025-12-07 04:11:58 +0700

    qwe

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,30 +18,33 @@ const montserrat = localFont({
 export const metadata = {
   icons: {
     icon: [
-      { rel: 'icon', type: 'image/svg+xml', url: '/favicon/favicon.svg' },
-      { rel: 'icon', type: 'image/png', sizes: '96x96', url: '/favicon/favicon-96x96.png' },
+      { rel: "icon", type: "image/svg+xml", url: "/favicon/favicon.svg" },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        url: "/favicon/favicon-96x96.png",
+      },
     ],
-    shortcut: '/favicon/favicon.ico',
-    apple: '/favicon/apple-touch-icon.png',
+    shortcut: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
   },
-  manifest: '/favicon/site.webmanifest',
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${montserrat.variable}`}>
-      <CartProvider>
-        <YandexMetrika />
-        <Bootstrap />
-        <Header />
-        <div className="newYearFon">
-          {children}
-        </div>
-        <Footer />
-        <Cart/>
-        <CartButton />
-        <BlockModal allowClose={true}/>
+        <CartProvider>
+          <YandexMetrika />
+          <Bootstrap />
+          <Header />
+          <div className="newYearFon">{children}</div>
+          <Footer />
+          <Cart />
+          <CartButton />
+          {/* <BlockModal allowClose={true}/> */}
         </CartProvider>
       </body>
     </html>
